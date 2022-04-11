1d683834bfb58c2080dbc4e42e226c7918b79ce7
David Gugea <David-Ioannis.Gugea@schwarz>
2022-04-11 14:20:29 +0200

    Finished employee data service ( get employee data by employee id ) & deleted exceptions thrown from the registerEmployee mapping

--- src/main/java/com/trackit/trackit/controller/UserController.java
+++ src/main/java/com/trackit/trackit/controller/UserController.java
@@ -2,6 +2,7 @@ package com.trackit.trackit.controller;
 
 
 import com.trackit.trackit.model.Employee;
+import com.trackit.trackit.service.EmployeeDataService;
 import com.trackit.trackit.service.EmployeeLoginService;
 import com.trackit.trackit.service.EmployeeRegisterService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +21,9 @@ public class UserController {
 
     @Autowired
     private final EmployeeRegisterService employeeRegisterService;
+
+    @Autowired
+    private final EmployeeDataService employeeDataService;
 
     @GetMapping(path = "api/employee/loginEmployee")
     public Employee getEmployeeId(
@@ -37,10 +40,18 @@ public class UserController {
             @RequestParam(value = "personnelNumber", required = true) String personnelNumber,
             @RequestParam(value = "firstName", required = true) String firstName,
             @RequestParam(value = "lastName", required = true) String lastName
-    ) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    ) {
         // api/employee/registerEmployee(username, password, personnelNumber, firstName, lastName)
         // returns true if the registration has been successful
         // returns false if the registration has failed
         return employeeRegisterService.registerNewEmployee(username, password, personnelNumber, firstName, lastName);
+    }
+
+    @GetMapping(path = "api/employee/getEmployeeDataByEmployeeId")
+    public Optional<Employee> getEmployeeData(
+            @RequestParam(value = "employee_id", required = true) Long employeeId
+    ){
+
+        return employeeDataService.getEmployeeDataByEmployeeId(employeeId);
     }
 }
